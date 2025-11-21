cf66032aa49820a31304e87d9944ac8e9beeed8d
Yasobaka Tusobaka <yarchuk.egor@gmail.com>
2025-11-21 02:29:26 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function summ(firstNumber, secondNumber){
-    firstNumber = Number(firstNumber);
-    secondNumber = Number(secondNumber);
-    result = firstNumber + secondNumber;
+    a = Number(firstNumber);
+    b = Number(secondNumber);
+    result = a + b;
     return alert(result);
 };
 
-const firstNumberRef = document.querySelector('input[name="firstNumber"]');
-const secondNumberRef = document.querySelector('input[name="secondNumber"]');
-const buttonRef = document.querySelector('button');
+const firstNumberRef = document.querySelector('#a');
+const secondNumberRef = document.querySelector('#b');
+const buttonRef = document.querySelector('#sumBtn');
 buttonRef.addEventListener('click', () => summ(firstNumberRef.value,secondNumberRef.value) );
