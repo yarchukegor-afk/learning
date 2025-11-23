73ab3579d50f2e30c15d30f61af83a4d66d904cc
Yasobaka Tusobaka <yarchuk.egor@gmail.com>
2025-11-23 23:28:29 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-function summ(firstNumber, secondNumber){
-    a = Number(firstNumber);
-    b = Number(secondNumber);
-    result = a + b;
-    return alert(result);
+const value = "Я отримав твій текст. Ось він : ";
+const userMessageRef = document.querySelector('#textFromUser')
+let buttonRef = document.querySelector('#confirm');
+function hello(hell2,massage){
+ return alert(value+massage);
 };
-
-const firstNumberRef = document.querySelector('#a');
-const secondNumberRef = document.querySelector('#b');
-const buttonRef = document.querySelector('#sumBtn');
-buttonRef.addEventListener('click', () => summ(firstNumberRef.value,secondNumberRef.value) );
+buttonRef.addEventListener('click' ,() => hello(value, userMessageRef.value));
--- test.js
+++
@@ -1,4 +0,0 @@
-let counter = 0;
-console.log( 2 * counter++ );
-counter++;
-console.log(counter);
